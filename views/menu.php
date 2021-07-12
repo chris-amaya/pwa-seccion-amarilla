d8aaa2f445a3a1a384c230eabf66a3a81c284447
chris <Chris Amaya>
2021-07-12 02:39:57 -0500

    ADD: link del repositorio de github

--- views/menu.php
+++ views/menu.php
@@ -1,13 +1,11 @@
 <div class="mdl-layout__drawer">
-    <nav class="mdl-navigation">
     <div class="side-nav__header">
-        <h1 class="side-nav__title">PWA</h1>
+        <h1 class="side-nav__title">Menu</h1>
     </div>
     <div class="side-nav__body">
         <a href="index.php" class="side-nav__home">Inicio</a>
         <a href="new-enterprise/" class="side-nav__add">Agregar</a>
         <a href="login.php" class="side-nav__login">Login</a>
     </div>
-    <div class="side-nav__version">Beta Version</div>
-    </nav>
+    <div class="side-nav__version"><a href="https://github.com/chris-amaya/pwa-seccion-amarilla">Link del repositorio</a></div>
 </div>
